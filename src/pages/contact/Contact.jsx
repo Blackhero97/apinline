5b23e4493e38b17270c07ce53136c7e17c5bc606
ilhomjon Solijonov <ilhomjonsolijonov843@gmail.com>
2024-06-09 13:22:50 +0500

    Added to contact send message to telegram function

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import { useState } from "react";
 import "./contact.css";
+import axios from "axios";
+
 
 function Contact() {
+  var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.getMonth() + 1; // Month is zero-based, so we add 1
+    var year = currentDate.getFullYear();
+    var formattedDate = day + "/" + month + "/ " + year;
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const sendMessageToTelegram = (e) => {
+      e.preventDefault();
+      setErrorMessage('');
+
+      const telegramMessage = `New Message!\n\nFirst Name: ${firstName}\nLast Name: ${lastName}\nEmail: ${email}\nPhone: ${phone}\nSubject: ${subject}\nMessage: ${message}\nDate: ${formattedDate}`;
+
+      const token = '7408762382:AAFIQpx6se__8tnnLy0TkjQbSySapY9FKIA';
+      const chatId = -1002197848312;
+
+      axios.post(`https://api.telegram.org/bot${token}/sendMessage`, {
+          chat_id: chatId,
+          text: telegramMessage
+      })
+          .then(response => {
+              alert('Message sent to Telegram:', response.data);
+              setFirstName('');
+              setLastName('');
+              setEmail('');
+              setPhone('');
+              setSubject('');
+              setMessage('');
+          })
+          // .catch(error => {
+          //     setErrorMessage('Error sending message to Telegram. Please try again later.');
+          // });
+      };
   return (
     <>
       <div className="contact page-padding">
@@ -17,37 +59,40 @@ function Contact() {
               <h3>Our Commitment to You</h3>
               <ul>
                 <li>
-                  <i class="bx bxs-badge-check"></i>Swift responses, dedicated
+                  <i className="bx bxs-badge-check"></i>Swift responses, dedicated
                   support
                 </li>
                 <li>
-                  <i class="bx bxs-badge-check"></i>Efficient and always here
+                  <i className="bx bxs-badge-check"></i>Efficient and always here
                   for you
                 </li>
                 <li>
-                  <i class="bx bxs-badge-check"></i>We listen, understand, and
+                  <i className="bx bxs-badge-check"></i>We listen, understand, and
                   act promptly
                 </li>
               </ul>
             </div>
           </div>
           <div className="contect-box-form card-padding">
-            <form action="">
+            <form action="" onSubmit={sendMessageToTelegram}>
               <div className="input-boxes">
                 <div className="input-box">
                   <label htmlFor="">First Name</label>
-                  <input required placeholder="Enter First Name" type="text" />
+                  <input required placeholder="Enter First Name" type="text" value={firstName}
+                                    onChange={e => setFirstName(e.target.value)} />
                 </div>
                 <div className="input-box">
                   {" "}
                   <label htmlFor="">Last Name</label>
-                  <input required placeholder="Enter Last Name" type="text" />
+                  <input required placeholder="Enter Last Name" type="text" value={lastName}
+                                    onChange={e => setLastName(e.target.value)} />
                 </div>
               </div>
               <div className="input-boxes">
                 <div className="input-box">
                   <label htmlFor="">Email</label>
-                  <input required placeholder="Enter your Email" type="text" />
+                  <input required placeholder="Enter your Email" type="email" value={email}
+                                    onChange={e => setEmail(e.target.value)} />
                 </div>
                 <div className="input-box">
                   {" "}
@@ -55,7 +100,9 @@ function Contact() {
                   <input
                     required
                     placeholder="Enter Phone Number"
-                    type="text"
+                    type="number"
+                    value={phone}
+                    onChange={e => setPhone(e.target.value)}
                   />
                 </div>
               </div>
@@ -66,6 +113,8 @@ function Contact() {
                     required
                     placeholder="Enter your Subject"
                     type="text"
+                    value={subject}
+                    onChange={e => setSubject(e.target.value)}
                   />
                 </div>
               </div>
@@ -79,12 +128,15 @@ function Contact() {
                     id=""
                     cols="30"
                     rows="10"
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
                   ></textarea>
                 </div>
               </div>
+              {errorMessage && <p className="text-center pt-2.5 text-red-500">{errorMessage}</p>}
               <div className="form-btn-box">
-                <button className="primary-btn">
-                  <i class="bx bxs-send"></i>Send Your Message
+                <button className="primary-btn" type="submit">
+                  <i className="bx bxs-send"></i>Send Your Message
                 </button>
               </div>
             </form>
@@ -97,7 +149,7 @@ function Contact() {
               <h4>apinline@gmail.com</h4>
             </div>
             <div className="contact-info-card-right">
-              <i class="bx bxs-chevron-right-circle"></i>
+              <i className="bx bxs-chevron-right-circle"></i>
             </div>
           </div>
           <div className="contact-info-card card-padding">
@@ -106,7 +158,7 @@ function Contact() {
               <h4>+998(88)-371-52-71</h4>
             </div>
             <div className="contact-info-card-right">
-              <i class="bx bxs-chevron-right-circle"></i>
+              <i className="bx bxs-chevron-right-circle"></i>
             </div>
           </div>
           <div className="contact-info-card card-padding">
@@ -115,7 +167,7 @@ function Contact() {
               <h4>9:00 am - 6:00 pm</h4>
             </div>
             <div className="contact-info-card-right">
-              <i class="bx bxs-chevron-right-circle"></i>
+              <i className="bx bxs-chevron-right-circle"></i>
             </div>
           </div>
         </div>
